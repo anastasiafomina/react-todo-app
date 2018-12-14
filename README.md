f63bf7ee71b3dd9d5a549db580acd0a1ad69a299
Anastasia Fomina <anastasia.o.fomina@gmail.com>
2018-12-14 15:40:29 +0300

    init commit-add structure

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+React-todo app
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,27 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './TodoList'
+import TodoItems from './TodoItems'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [],
+      currentItem: {
+        text: '', 
+        key: ''
+      },
+    }
+  }
+
+  handleInput = e => {
+    const itemText = e.target.value
+    const currentItem = { text: itemText, key: Date.now() }
+    this.setState({
+      currentItem,
+    })
+  }
+
+  addItem = e => {
+    e.preventDefault()
+    const newItem = this.state.currentItem
+    if (newItem.text !== '') {
+      const items = [...this.state.items, newItem]
+      this.setState({
+        items: items,
+        currentItem: { text: '', key: '' },
+      })
+    }
+  }
+
+  deleteItem = key => {
+    const filteredItems = this.state.items.filter(item => {
+      return item.key !== key
+    })
+    this.setState({
+      items: filteredItems,
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <TodoList 
+          addItem={this.addItem} 
+          handleInput={this.handleInput}
+          currentItem={this.state.currentItem}
+        />
+        <TodoItems 
+          entries={this.state.items}
+          deleteItem={this.deleteItem}
+         />
       </div>
-    );
+    )
   }
 }
 
