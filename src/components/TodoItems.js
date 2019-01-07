d7907f521b8d85057bc3405f86bea433fa1f053e
Anastasia Fomina <anastasia.o.fomina@gmail.com>
2019-01-08 01:35:16 +0300

    add date and time of task adding

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react'
 
 class TodoItems extends Component {
   renderTask = item => {
+    const date = new Date(item.date)
+    const months = ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек']
+    function getCurrentMonth () {
+      const index = date.getMonth()
+      return months[index]
+    }
+    const minutes = (date.getMinutes() < 10) ? ("0" + date.getMinutes()) : date.getMinutes()
+    const hours = (date.getHours() < 10) ? ("0" + date.getHours()) : date.getHours()
+
+    const itemDateTime = date.getDate() + ' ' + getCurrentMonth() + ', ' + hours + ':' + minutes
+    
     return (
       <div 
         className="taskLine"
         key={item.key} 
       >
+        <div className="itemDateTime">
+          {itemDateTime}
+        </div>
         <li 
           onClick={() => this.props.crossOutOnClick(item.key)}
           className={ (item.isDone) ? 'crossOut' : '' }
